d22cfa8e75942d95f9140dd10cc0e602ef19fce6
Myk Melez <myk@mozilla.org>
2008-02-05 01:26:06 -0800

    define resource protocol alias in chrome.manifest and use it to import modules

---
+++ extension/content/init.js
@@ -0,0 +1,8 @@
+if (typeof Cc == "undefined") var Cc = Components.classes;
+if (typeof Ci == "undefined") var Ci = Components.interfaces;
+if (typeof Cr == "undefined") var Cr = Components.results;
+if (typeof Cu == "undefined") var Cu = Components.utils;
+
+Cu.import("resource://snowl/service.js");
+Cu.import("resource://snowl/datastore.js");
+Cu.import("resource://snowl/log4moz.js");
