5ca2a31cb45cd347a2e8b660ad5c3f84998db2cc
Myk Melez <myk@mozilla.org>
2008-04-15 16:49:55 -0700

    feedclient.js is now the feed.js module

--- extension/content/subscribe.js
+++ extension/content/subscribe.js
@@ -3,9 +3,10 @@ const Ci = Components.interfaces;
 const Cr = Components.results;
 const Cu = Components.utils;
 
+Cu.import("resource://snowl/log4moz.js");
 Cu.import("resource://snowl/service.js");
 Cu.import("resource://snowl/datastore.js");
-Cu.import("resource://snowl/log4moz.js");
+Cu.import("resource://snowl/feed.js");
 
 let Subscriber = {
   // Observer Service
