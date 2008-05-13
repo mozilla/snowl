2e0b4320eb1a806e30300df120a9e7a7f1b1699c
Myk Melez <myk@mozilla.org>
2008-05-12 23:19:55 -0700

    combine the river writer with the river view controller

--- extension/content/river.js
+++ extension/content/river.js
@@ -42,15 +42,62 @@ const Cr = Components.results;
 const Cu = Components.utils;
 
 Cu.import("resource://snowl/modules/collection.js");
-Cu.import("resource://snowl/modules/RiverWriter.js");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://snowl/modules/log4moz.js");
+
+let log = Log4Moz.Service.getLogger("Snowl.River");
+
+/**
+ * Wrapper function for nsIIOService::newURI.
+ * @param aURLSpec
+ *        The URL string from which to create an nsIURI.
+ * @returns an nsIURI object, or null if the creation of the URI failed.
+ */
+function makeURI(aURLSpec, aCharset) {
+  var ios = Cc["@mozilla.org/network/io-service;1"].
+            getService(Ci.nsIIOService);
+  try {
+    return ios.newURI(aURLSpec, aCharset, null);
+  } catch (ex) { }
+
+  return null;
+}
+
+const XML_NS = "http://www.w3.org/XML/1998/namespace"
+const HTML_NS = "http://www.w3.org/1999/xhtml";
+const URI_BUNDLE = "chrome://browser/locale/feeds/subscribe.properties";
+
+const TITLE_ID = "feedTitleText";
+const SUBTITLE_ID = "feedSubtitleText";
+
+/**
+ * Converts a number of bytes to the appropriate unit that results in a
+ * number that needs fewer than 4 digits
+ *
+ * @return a pair: [new value with 3 sig. figs., its unit]
+  */
+function convertByteUnits(aBytes) {
+  var units = ["bytes", "kilobyte", "megabyte", "gigabyte"];
+  let unitIndex = 0;
+ 
+  // convert to next unit if it needs 4 digits (after rounding), but only if
+  // we know the name of the next unit
+  while ((aBytes >= 999.5) && (unitIndex < units.length - 1)) {
+    aBytes /= 1024;
+    unitIndex++;
+  }
+ 
+  // Get rid of insignificant bits by truncating to 1 or 0 decimal points
+  // 0 -> 0; 1.2 -> 1.2; 12.3 -> 12.3; 123.4 -> 123; 234.5 -> 235
+  aBytes = aBytes.toFixed((aBytes > 0) && (aBytes < 100) ? 1 : 0);
+ 
+  return [aBytes, units[unitIndex]];
+}
 
 var RiverView = {
   // The set of messages to display in the view.
   _collection: null,
   
-  // The object that generates the view.
-  _riverWriter: null,
-
   init: function SH_init() {
     this._params = {};
     let query = window.location.search.substr(1);
@@ -70,13 +117,18 @@ var RiverView = {
       this._collection.read = false;
       document.getElementById("unreadButton").checked = true;
     }
-    this._riverWriter = new SnowlRiverWriter();
-    this._riverWriter.init(window, this._collection);
-    this._riverWriter.writeContent();
+
+    // Explicitly wrap |window| in an XPCNativeWrapper to make sure
+    // it's a real native object! This will throw an exception if we
+    // get a non-native object.
+    this._window = new XPCNativeWrapper(window);
+    this._document = this._window.document;
+
+    this.writeContent();
   },
 
   uninit: function SH_uninit() {
-    this._riverWriter.close();
+    this.close();
   },
 
   onCommandUnreadButton: function(aEvent, aButton) {
@@ -86,11 +138,11 @@ var RiverView = {
 
     if (aButton.checked) {
       this._collection.read = false;
-      this._riverWriter.rebuildView();
+      this.rebuildView();
     }
     else {
       this._collection.read = undefined;
-      this._riverWriter.rebuildView();
+      this.rebuildView();
     }
 
     let query = aButton.checked ? "?unread" : "";
@@ -130,7 +182,424 @@ var RiverView = {
         this._collection.messages[i].read = true;
     }
 
+  },
+
+
+
+  get _log() {
+    let log = Log4Moz.Service.getLogger("Snowl.River");
+    this.__defineGetter__("_log", function() { return log });
+    return this._log;
+  },
+
+  _mimeSvc      : Cc["@mozilla.org/mime;1"].
+                  getService(Ci.nsIMIMEService),
+
+  _setContentText: function FW__setContentText(id, text) {
+    this._contentSandbox.element = this._document.getElementById(id);
+    this._contentSandbox.textNode = this._document.createTextNode(text);
+    var codeStr =
+      "while (element.hasChildNodes()) " +
+      "  element.removeChild(element.firstChild);" +
+      "element.appendChild(textNode);";
+    Cu.evalInSandbox(codeStr, this._contentSandbox);
+    this._contentSandbox.element = null;
+    this._contentSandbox.textNode = null;
+  },
+
+  /**
+   * Safely sets the href attribute on an anchor tag, providing the URI 
+   * specified can be loaded according to rules.
+   *
+   * XXX Renamed from safeSetURIAttribute to unsafeSetURIAttribute to reflect
+   * that we've commented out the stuff that makes it safe.
+   *
+   * FIXME: I don't understand the security implications here, but presumably
+   * there's a reason this is here, and we should be respecting it, so make this
+   * work by giving each message in a collection have a reference to its source
+   * and then use the source's URI to create the principal with which we compare
+   * the URI.
+   * 
+   * @param   element
+   *          The element to set a URI attribute on
+   * @param   attribute
+   *          The attribute of the element to set the URI to, e.g. href or src
+   * @param   uri
+   *          The URI spec to set as the href
+   */
+  _unsafeSetURIAttribute: 
+  function FW__unsafeSetURIAttribute(element, attribute, uri) {
+/*
+    var secman = Cc["@mozilla.org/scriptsecuritymanager;1"].
+                 getService(Ci.nsIScriptSecurityManager);    
+    const flags = Ci.nsIScriptSecurityManager.DISALLOW_INHERIT_PRINCIPAL;
+    try {
+      secman.checkLoadURIStrWithPrincipal(this._feedPrincipal, uri, flags);
+      // checkLoadURIStrWithPrincipal will throw if the link URI should not be
+      // loaded, either because our feedURI isn't allowed to load it or per
+      // the rules specified in |flags|, so we'll never "linkify" the link...
+    }
+    catch (e) {
+      // Not allowed to load this link because secman.checkLoadURIStr threw
+      return;
+    }
+*/
+
+    this._contentSandbox.element = element;
+    this._contentSandbox.uri = uri;
+    var codeStr = "element.setAttribute('" + attribute + "', uri);";
+    Cu.evalInSandbox(codeStr, this._contentSandbox);
+  },
+
+  /**
+   * Use this sandbox to run any dom manipulation code on nodes which
+   * are already inserted into the content document.
+   */
+  __contentSandbox: null,
+  get _contentSandbox() {
+    if (!this.__contentSandbox)
+      this.__contentSandbox = new Cu.Sandbox(this._window);
+
+    return this.__contentSandbox;
+  },
+
+  __bundle: null,
+  get _bundle() {
+    if (!this.__bundle) {
+      this.__bundle = Cc["@mozilla.org/intl/stringbundle;1"].
+                      getService(Ci.nsIStringBundleService).
+                      createBundle(URI_BUNDLE);
+    }
+    return this.__bundle;
+  },
+
+  _getFormattedString: function FW__getFormattedString(key, params) {
+    return this._bundle.formatStringFromName(key, params, params.length);
+  },
+  
+  _getString: function FW__getString(key) {
+    return this._bundle.GetStringFromName(key);
+  },
+
+   /**
+   * Returns a date suitable for displaying in the feed preview. 
+   * If the date cannot be parsed, the return value is "false".
+   * @param   dateString
+   *          A date as extracted from a feed entry. (entry.updated)
+   */
+  _parseDate: function FW__parseDate(dateString) {
+    // Convert the date into the user's local time zone
+    dateObj = new Date(dateString);
+
+    // Make sure the date we're given is valid.
+    if (!dateObj.getTime())
+      return false;
+
+    var dateService = Cc["@mozilla.org/intl/scriptabledateformat;1"].
+                      getService(Ci.nsIScriptableDateFormat);
+    return dateService.FormatDateTime("", dateService.dateFormatLong, dateService.timeFormatNoSeconds,
+                                      dateObj.getFullYear(), dateObj.getMonth()+1, dateObj.getDate(),
+                                      dateObj.getHours(), dateObj.getMinutes(), dateObj.getSeconds());
+  },
+
+  /**
+   * Writes the feed title into the preview document.
+   * @param   container
+   *          The feed container
+   */
+  _setTitleText: function FW__setTitleText(container) {
+    if (container.title) {
+      this._setContentText(TITLE_ID, container.title.plainText());
+      this._document.title = container.title.plainText();
+    }
+
+    var feed = container.QueryInterface(Ci.nsIFeed);
+    if (feed && feed.subtitle)
+      this._setContentText(SUBTITLE_ID, container.subtitle.plainText());
+  },
+
+  /**
+   * Writes the title image into the preview document if one is present.
+   * @param   container
+   *          The feed container
+   */
+  _setTitleImage: function FW__setTitleImage(container) {
+    try {
+      var parts = container.image;
+      
+      // Set up the title image (supplied by the feed)
+      var feedTitleImage = this._document.getElementById("feedTitleImage");
+      this._unsafeSetURIAttribute(feedTitleImage, "src", 
+                                parts.getPropertyAsAString("url"));
+
+      // Set up the title image link
+      var feedTitleLink = this._document.getElementById("feedTitleLink");
+
+      var titleText = this._getFormattedString("linkTitleTextFormat", 
+                                               [parts.getPropertyAsAString("title")]);
+      this._contentSandbox.feedTitleLink = feedTitleLink;
+      this._contentSandbox.titleText = titleText;
+      var codeStr = "feedTitleLink.setAttribute('title', titleText);";
+      Cu.evalInSandbox(codeStr, this._contentSandbox);
+      this._contentSandbox.feedTitleLink = null;
+      this._contentSandbox.titleText = null;
+
+      this._unsafeSetURIAttribute(feedTitleLink, "href", 
+                                parts.getPropertyAsAString("link"));
+
+      // Fix the margin on the main title, so that the image doesn't run over
+      // the underline
+      var feedTitleText = this._document.getElementById("feedTitleText");
+      var titleImageWidth = parseInt(parts.getPropertyAsAString("width")) + 15;
+      feedTitleText.style.marginRight = titleImageWidth + "px";
+    }
+    catch (e) {
+      this._log.info("Failed to set Title Image (this is benign): " + e);
+    }
+  },
+
+  /**
+   * Writes all entries contained in the feed.
+   * @param   container
+   *          The container of entries in the feed
+   */
+  _writeFeedContent: function FW__writeFeedContent() {
+    // Build the actual feed content
+    //var feed = container.QueryInterface(Ci.nsIFeed);
+    //if (feed.items.length == 0)
+    //  return;
+
+    this._contentSandbox.feedContent =
+      this._document.getElementById("feedContent");
+
+    for (let i = 0; i < this._collection.messages.length; ++i) {
+      let entry = this._collection.messages[i];
+
+      var entryContainer = this._document.createElementNS(HTML_NS, "div");
+      entryContainer.className = "entry";
+      entryContainer.setAttribute("index", i);
+
+      // If the entry has a title, make it a link
+      if (entry.subject) {
+        var a = this._document.createElementNS(HTML_NS, "a");
+        a.appendChild(this._document.createTextNode(entry.source.title));
+        a.appendChild(this._document.createTextNode(": "));
+        a.appendChild(this._document.createTextNode(entry.author));
+        a.appendChild(this._document.createTextNode(": "));
+        a.appendChild(this._document.createTextNode(entry.subject));
+
+        // Entries are not required to have links, so entry.link can be null.
+        if (entry.link)
+          this._unsafeSetURIAttribute(a, "href", entry.link);
+
+        var title = this._document.createElementNS(HTML_NS, "h3");
+        title.appendChild(a);
+
+        var lastUpdated = this._parseDate(entry.timestamp);
+        if (lastUpdated) {
+          var dateDiv = this._document.createElementNS(HTML_NS, "div");
+          dateDiv.className = "lastUpdated";
+          dateDiv.textContent = lastUpdated;
+          title.appendChild(dateDiv);
+        }
+
+        entryContainer.appendChild(title);
+      }
+
+      var body = this._document.createElementNS(HTML_NS, "div");
+
+      // The summary is currently not stored and made available, so we can
+      // only use the content.
+      // FIXME: use the summary instead once it becomes available.
+      //var summary = entry.summary || entry.content;
+      var summary = entry.content;
+
+      var docFragment = null;
+      if (summary) {
+        if (summary.base)
+          body.setAttributeNS(XML_NS, "base", summary.base.spec);
+        docFragment = summary.createDocumentFragment(body);
+        if (docFragment)
+          body.appendChild(docFragment);
+
+        // If the entry doesn't have a title, append a # permalink
+        // See http://scripting.com/rss.xml for an example
+        if (!entry.subject && entry.link) {
+          var a = this._document.createElementNS(HTML_NS, "a");
+          a.appendChild(this._document.createTextNode("#"));
+          this._unsafeSetURIAttribute(a, "href", entry.link);
+          body.appendChild(this._document.createTextNode(" "));
+          body.appendChild(a);
+        }
+
+      }
+      body.className = "feedEntryContent";
+      entryContainer.appendChild(body);
+
+      if (entry.enclosures && entry.enclosures.length > 0) {
+        var enclosuresDiv = this._buildEnclosureDiv(entry);
+        entryContainer.appendChild(enclosuresDiv);
+      }
+
+      this._contentSandbox.entryContainer = entryContainer;
+      this._contentSandbox.clearDiv =
+        this._document.createElementNS(HTML_NS, "div");
+      this._contentSandbox.clearDiv.style.clear = "both";
+      
+      var codeStr = "feedContent.appendChild(entryContainer); " +
+                     "feedContent.appendChild(clearDiv);"
+      Cu.evalInSandbox(codeStr, this._contentSandbox);
+    }
+
+    this._contentSandbox.feedContent = null;
+    this._contentSandbox.entryContainer = null;
+    this._contentSandbox.clearDiv = null;
+  },
+
+  /**
+   * Takes a url to a media item and returns the best name it can come up with.
+   * Frequently this is the filename portion (e.g. passing in 
+   * http://example.com/foo.mpeg would return "foo.mpeg"), but in more complex
+   * cases, this will return the entire url (e.g. passing in
+   * http://example.com/somedirectory/ would return 
+   * http://example.com/somedirectory/).
+   * @param aURL
+   *        The URL string from which to create a display name
+   * @returns a string
+   */
+  _getURLDisplayName: function FW__getURLDisplayName(aURL) {
+    var url = makeURI(aURL);
+    url.QueryInterface(Ci.nsIURL);
+    if (url == null || url.fileName.length == 0)
+      return aURL;
+
+    return decodeURI(url.fileName);
+  },
+
+  /**
+   * Takes a FeedEntry with enclosures, generates the HTML code to represent
+   * them, and returns that.
+   * @param   entry
+   *          FeedEntry with enclosures
+   * @returns element
+   */
+  _buildEnclosureDiv: function FW__buildEnclosureDiv(entry) {
+    var enclosuresDiv = this._document.createElementNS(HTML_NS, "div");
+    enclosuresDiv.className = "enclosures";
+
+    enclosuresDiv.appendChild(this._document.createTextNode(this._getString("mediaLabel")));
+
+    var roundme = function(n) {
+      return (Math.round(n * 100) / 100).toLocaleString();
+    }
+
+    for (var i_enc = 0; i_enc < entry.enclosures.length; ++i_enc) {
+      var enc = entry.enclosures.queryElementAt(i_enc, Ci.nsIWritablePropertyBag2);
+
+      if (!(enc.hasKey("url"))) 
+        continue;
+
+      var enclosureDiv = this._document.createElementNS(HTML_NS, "div");
+      enclosureDiv.setAttribute("class", "enclosure");
+
+      var mozicon = "moz-icon://.txt?size=16";
+      var type_text = null;
+      var size_text = null;
+
+      if (enc.hasKey("type")) {
+        type_text = enc.get("type");
+        try {
+          var handlerInfoWrapper = this._mimeSvc.getFromTypeAndExtension(enc.get("type"), null);
+
+          if (handlerInfoWrapper)
+            type_text = handlerInfoWrapper.description;
+
+          if  (type_text && type_text.length > 0)
+            mozicon = "moz-icon://goat?size=16&contentType=" + enc.get("type");
+
+        } catch (ex) { }
+
+      }
+
+      if (enc.hasKey("length") && /^[0-9]+$/.test(enc.get("length"))) {
+        var enc_size = convertByteUnits(parseInt(enc.get("length")));
+
+        var size_text = this._getFormattedString("enclosureSizeText", 
+                             [enc_size[0], this._getString(enc_size[1])]);
+      }
+
+      var iconimg = this._document.createElementNS(HTML_NS, "img");
+      iconimg.setAttribute("src", mozicon);
+      iconimg.setAttribute("class", "type-icon");
+      enclosureDiv.appendChild(iconimg);
+
+      enclosureDiv.appendChild(this._document.createTextNode( " " ));
+
+      var enc_href = this._document.createElementNS(HTML_NS, "a");
+      enc_href.appendChild(this._document.createTextNode(this._getURLDisplayName(enc.get("url"))));
+      this._unsafeSetURIAttribute(enc_href, "href", enc.get("url"));
+      enclosureDiv.appendChild(enc_href);
+
+      if (type_text && size_text)
+        enclosureDiv.appendChild(this._document.createTextNode( " (" + type_text + ", " + size_text + ")"));
+
+      else if (type_text) 
+        enclosureDiv.appendChild(this._document.createTextNode( " (" + type_text + ")"))
+
+      else if (size_text)
+        enclosureDiv.appendChild(this._document.createTextNode( " (" + size_text + ")"))
+ 
+      enclosuresDiv.appendChild(enclosureDiv);
+    }
+
+    return enclosuresDiv;
+  },
+
+  _window: null,
+  _document: null,
+  _feedURI: null,
+  _feedPrincipal: null,
+
+  writeContent: function FW_writeContent() {
+    if (!this._window)
+      return;
+
+    try {
+      // Set up the feed content
+      //var container = this._getContainer();
+      //if (!container)
+      //  return;
+
+      //this._setTitleText(container);
+      //this._setTitleImage(container);
+      this._writeFeedContent();
+    }
+    finally {
+      //this._removeFeedFromCache();
+    }
+  },
+
+  rebuildView: function() {
+    let feedContent = this._document.getElementById("feedContent");
+    while (feedContent.hasChildNodes())
+      feedContent.removeChild(feedContent.lastChild);
+
+    this.writeContent();
+  },
+
+  close: function FW_close() {
+    this._document = null;
+    this._window = null;
+
+    this.__bundle = null;
+    //this._feedURI = null;
+    this.__contentSandbox = null;
+
+    var historySvc = Cc["@mozilla.org/browser/nav-history-service;1"].
+                     getService(Ci.nsINavHistoryService);
+    historySvc.removeObserver(this);
   }
+
 };
 
 window.addEventListener("scroll", function(evt) RiverView.onScroll(evt), false);
