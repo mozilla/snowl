7d42bdbe0ae9b216f0543db35feaff85b833049e
Myk Melez <myk@mozilla.org>
2008-06-14 22:58:07 -0700

    display timestamps in the message view, and display message titles in the titlebar

--- extension/content/message/message.js
+++ extension/content/message/message.js
@@ -39,9 +39,69 @@ if (content) {
 
 document.getElementById("author").value = message.author;
 document.getElementById("subject").value = message.subject;
-document.getElementById("timestamp").value = message.timestamp;
+document.documentElement.setAttribute("title", message.subject);
+document.getElementById("timestamp").value = formatTimestamp(new Date(message.timestamp));
 
 let link = document.createElementNS(HTML_NS, "a");
 link.setAttribute("href", message.link);
 link.appendChild(document.createTextNode(message.link));
 document.getElementById("link").appendChild(link);
+
+// FIXME: put this into a SnowlUtils module.
+
+/**
+ * Formats a timestamp for human consumption using the date formatting service
+ * for locale-specific formatting along with some additional smarts for more
+ * human-readable representations of recent timestamps.
+ * @param   {Date} the timestamp to format
+ * @returns a human-readable string
+ */
+function formatTimestamp(aTimestamp) {
+  let formattedString;
+
+  let dfSvc = Cc["@mozilla.org/intl/scriptabledateformat;1"].
+              getService(Ci.nsIScriptableDateFormat);
+
+  let now = new Date();
+
+  let yesterday = new Date(now - 24 * 60 * 60 * 1000);
+  yesterday = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate());
+
+  let sixDaysAgo = new Date(now - 6 * 24 * 60 * 60 * 1000);
+  sixDaysAgo = new Date(sixDaysAgo.getFullYear(), sixDaysAgo.getMonth(), sixDaysAgo.getDate());
+
+  if (aTimestamp.toLocaleDateString() == now.toLocaleDateString())
+    formattedString = dfSvc.FormatTime("",
+                                             dfSvc.timeFormatNoSeconds,
+                                             aTimestamp.getHours(),
+                                             aTimestamp.getMinutes(),
+                                             null);
+  else if (aTimestamp > yesterday)
+    formattedString = "Yesterday " + dfSvc.FormatTime("",
+                                                            dfSvc.timeFormatNoSeconds,
+                                                            aTimestamp.getHours(),
+                                                            aTimestamp.getMinutes(),
+                                                            null);
+  else if (aTimestamp > sixDaysAgo)
+    formattedString = dfSvc.FormatDateTime("",
+                                                 dfSvc.dateFormatWeekday, 
+                                                 dfSvc.timeFormatNoSeconds,
+                                                 aTimestamp.getFullYear(),
+                                                 aTimestamp.getMonth() + 1,
+                                                 aTimestamp.getDate(),
+                                                 aTimestamp.getHours(),
+                                                 aTimestamp.getMinutes(),
+                                                 aTimestamp.getSeconds());
+  else
+    formattedString = dfSvc.FormatDateTime("",
+                                                 dfSvc.dateFormatShort, 
+                                                 dfSvc.timeFormatNoSeconds,
+                                                 aTimestamp.getFullYear(),
+                                                 aTimestamp.getMonth() + 1,
+                                                 aTimestamp.getDate(),
+                                                 aTimestamp.getHours(),
+                                                 aTimestamp.getMinutes(),
+                                                 aTimestamp.getSeconds());
+
+  return formattedString;
+}
