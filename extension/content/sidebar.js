8222edcf75e0fc00d4f341f913eb1773a18778f8
Myk Melez <myk@mozilla.org>
2008-07-20 21:11:26 -0700

    move the collections view into an overlay to facilitate its reuse inside the river view

---
+++ extension/content/sidebar.js
@@ -0,0 +1,18 @@
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+let gBrowserWindow = window.QueryInterface(Ci.nsIInterfaceRequestor).
+                     getInterface(Ci.nsIWebNavigation).
+                     QueryInterface(Ci.nsIDocShellTreeItem).
+                     rootTreeItem.
+                     QueryInterface(Ci.nsIInterfaceRequestor).
+                     getInterface(Ci.nsIDOMWindow);
+
+let gMessageViewWindow = window.QueryInterface(Ci.nsIInterfaceRequestor).
+                         getInterface(Ci.nsIWebNavigation).
+                         QueryInterface(Ci.nsIDocShellTreeItem).
+                         rootTreeItem.
+                         QueryInterface(Ci.nsIInterfaceRequestor).
+                         getInterface(Ci.nsIDOMWindow);
