847f39678d58380dbdae9af4fb93f4bd3f238cc8
Myk Melez <myk@mozilla.org>
2008-07-28 07:54:36 -0700

    hide the list view by default but open it automatically when the user opens the sidebar

--- extension/content/sidebar.js
+++ extension/content/sidebar.js
@@ -16,3 +16,9 @@ let gMessageViewWindow = window.QueryInterface(Ci.nsIInterfaceRequestor).
                          rootTreeItem.
                          QueryInterface(Ci.nsIInterfaceRequestor).
                          getInterface(Ci.nsIDOMWindow);
+
+{
+  let snowlViewContainer = gBrowserWindow.document.getElementById("snowlViewContainer");
+  if (snowlViewContainer.hidden)
+    snowlViewContainer.hidden = false;
+}
