548693e330fcd6ceda0345118d01b3c88a1fa7af
Myk Melez <myk@mozilla.org>
2008-12-18 17:06:38 -0800

    sort the river view using the same sort order (message ID) as the stream view

--- content/river.js
+++ content/river.js
@@ -587,7 +587,7 @@ let SnowlMessageView = {
   setCollection: function(collection) {
     this._collection = collection;
     this._collection.sortOrder = -1;
-    this._collection.sortProperties = ["received", "timestamp"];
+    this._collection.sortProperties = ["id"];
     this._updateURI();
     this._applyFilters();
     // No need to rebuild the view here, as _applyFilters will do it for us.
