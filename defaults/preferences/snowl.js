5d90176422b7ea46aac0c344cdf644e7b4ffbf78
Myk Melez <myk@mozilla.org>
2008-12-31 15:50:04 -0800

    format the default preferences a bit better

--- defaults/preferences/snowl.js
+++ defaults/preferences/snowl.js
@@ -1,7 +1,9 @@
 // About box description, so it's localizable
 pref("extensions.snowl@mozilla.org.description", "chrome://snowl/locale/about.properties");
-// Header view pref: 0 = none, 1 = brief, 2= full
+
+// Header view pref: 0 = none, 1 = brief, 2 = full
 pref("extensions.snowl.message.headerView", 1);
+
 // Collections hierarchical view pref: 0 = flat, 1 = hierarchical
 pref("extensions.snowl.collection.hierarchicalView", 0);
 
