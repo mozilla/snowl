4f0baae54fbe15d85f58f458962319e2c7b7f09a
Myk Melez <myk@mozilla.org>
2009-04-14 15:23:07 -0700

    make test work by defining server globally

--- test/unit/test_feed.js
+++ test/unit/test_feed.js
@@ -7,16 +7,18 @@ Cu.import("resource://snowl/modules/URI.js");
 // Snowl-specific modules
 Cu.import("resource://snowl/modules/feed.js");
 
+let server;
+
 function run_test() {
-  let server = new nsHttpServer();
+  server = new nsHttpServer();
   let basePath = do_get_file("test/unit/");
   server.registerDirectory("/", basePath);
   server.start(8080);
 
   do_test_pending();
 
-  let feed = new SnowlFeed(null, null, new URI("http://localhost:8080/feed.xml"), undefined, null);
   Observers.add("snowl:subscribe:get:end", finish_test);
+  let feed = new SnowlFeed(null, null, new URI("http://localhost:8080/feed.xml"), undefined, null);
   feed.subscribe();
 }
 
